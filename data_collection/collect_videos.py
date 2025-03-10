aea9f891b3a38c475bdeb4a88d15a4da9fa174b0
aarushi-agarwal <122412413+aarushi-agarwal@users.noreply.github.com>
2025-03-10 18:30:16 +0530

    Create collect_videos.py

---
+++ data_collection/collect_videos.py
@@ -0,0 +1,10 @@
+"""
+
+
+
+yt-dlp --cookies ~/www.hotstar.com_cookies.txt -f "bestvideo+bestaudio" --merge-output-format mp4 -o "ksbkbt_s1e9.mp4" "https://abc27znaaaaaaaamaaaaaaaaaaaaa.hses7-vod-cf-ace.cdn.hotstar.com/videos/hotstarint/1000014566/v3/1652713709727/651e1040045f6dee8363ad81f53de239/audio_aac_1652713709727/und/mp4a/index.m3u8"
+
+
+
+"""
+
